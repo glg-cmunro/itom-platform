a45df559acc6092b58509096c87ffecc6016037b
Chris Munro <chris@greenlightgroup.com>
2021-10-14 10:13:03 -0600

    tools updates

--- Clients/Schlumberger/GCP/gcloud_commands.sh
+++ Clients/Schlumberger/GCP/gcloud_commands.sh
@@ -22,6 +22,8 @@ gcloud config configurations activate slb-gke-p
 gcloud compute start-iap-tunnel gcp6133prdapp01 22 --local-host-port=localhost:22001 --zone=europe-west1-b --project=us102173-p-sis-bsys-6133
 gcloud compute start-iap-tunnel gcp6133prdapp02 3389 --local-host-port=localhost:23389 --zone=europe-west1-b --project=us102173-p-sis-bsys-6133
 
+gcloud compute ssh gcp6133prdapp01
+
 ## Non-Prod
 gcloud config configurations activate slb-gke-np
 gcloud compute start-iap-tunnel gcp6133tstapp04 22 --local-host-port=localhost:22004 --zone=europe-west1-b --project=us107795-np-sis-bsys-6133
@@ -30,7 +32,6 @@ gcloud compute start-iap-tunnel gcp6133tstapp05 3389 --local-host-port=localhost
 
 
 ##DR
-# EVT: 
 curl -k https://owncloud.greenlightgroup.com/index.php/s/9DotY6EJsYcovlY/download -o SMA_Operation_Toolkit_2020.11.zip
 unzip SMA_Operation_Toolkit_2020.11.zip
 mkdir -p /opt/smax/2020.11/tools/disaster-recovery
